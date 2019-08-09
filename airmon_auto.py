565b11fb7e6587e278b97d6ae78c3b100fa67822
Alex <alexzele.b@gmail.com>
2019-08-09 12:15:13 +0300

    test

--- airmon_auto.py
+++ airmon_auto.py
@@ -3,23 +3,22 @@ from time import sleep, time
 from wifi import Cell
 import subprocess
 
-print("This Project Have been done For School , you use its on our own resbonsiblity")
-time.sleep(2)
-os.system('clear')
-cell=Cell.all("wlan0")
-
-for x in xrange(0,len(cell)):
-    print ("The network name that aviable:" + str(cell[x]))
-
-#line creator
-cmd = ['echo', 'I like potatos']
-proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-o, e = proc.communicate()
-
-print('Output: ' + o.decode('ascii'))
-print('Error: '  + e.decode('ascii'))
-print('code: ' + str(proc.returncode))
 
 
+
+def main():
+
+    print("This Project Have been done For School , you use its on our own resbonsiblity")
+
+    cmd = ['airmon-ng start wlan0']
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    o, e = proc.communicate()
+
+    print('Output: ' + o)
+    print('Error: '  + e)
+
+
+if __name__ == '__main__':
+    main()
 
