524d19123b0e70b2eae3c58f88a9224cfae16d7f
Alex <alexzele.b@gmail.com>
2019-08-09 12:05:33 +0300

    test

--- airmon_auto.py
+++ airmon_auto.py
@@ -1,6 +1,7 @@
-from os import system, os
+import os
 from time import sleep, time
 from wifi import Cell
+import subprocess
 
 print("This Project Have been done For School , you use its on our own resbonsiblity")
 time.sleep(2)
@@ -10,8 +11,15 @@ cell=Cell.all("wlan0")
 for x in xrange(0,len(cell)):
     print ("The network name that aviable:" + str(cell[x]))
 
+#line creator
+cmd = ['echo', 'I like potatos']
+proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
+o, e = proc.communicate()
 
+print('Output: ' + o.decode('ascii'))
+print('Error: '  + e.decode('ascii'))
+print('code: ' + str(proc.returncode))
 
 
 
